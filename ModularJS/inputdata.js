886d96309d676ac3420b0429dd6933c4a21b524a
abdisavia <ridhodanabdi7a@gmail.com>
2023-07-10 17:23:31 +0700

    Updated, add private function in inputdata.js

--- ModularJS/inputdata.js
+++ ModularJS/inputdata.js
@@ -1,54 +1,66 @@
-(function(){
-    var pekerja = {
-        Pekerja :[{"Nama" : "abdi", "Gaji" : "20000000"}],
-        init : function(){
-            this.tempDOM()
-            this.aksi()
-            this.tampilkan()
-        },
-        tempDOM : function(){
-            this.inputGaji = document.querySelector("#inputGaji")
-            this.inputNama = document.querySelector("#inputName")
-            this.btnSubmit = document.querySelector("#btnSubmit")
-            this.template = document.querySelector("#template")
-            this.table = document.querySelector("#output")
-            this.tbody = this.table.querySelector("#body")
-            this.tr = this.tbody.querySelectorAll("tr")
-            this.btnDelete = document.querySelectorAll(".delete")
-        },
-        pushKePekerja : function(){
-            let obj = {}
-            obj["Nama"] = this.inputNama.value
-            obj["Gaji"] = this.inputGaji.value
-            this.Pekerja.push(obj)
-            this.tampilkan()
-        },
-        deletePekerja : function(e,parent){
-            var i = e.target.closest("tr")
-            parent.removeChild(i)
-            this.Pekerja.splice(0,1)
-            this.tampilkan()
-        },
-        aksi : function(){
-            this.btnSubmit.addEventListener('click', this.pushKePekerja.bind(this))
-            this.tbody.addEventListener('click', function(e) {
-                if(e.target.nodeName = "BUTTON"){
-                    this.deletePekerja(e,this.tbody)
-                }
-            }.bind(this))
-        },
-        tampilkan : function(){
-            if(this.Pekerja.length != 0){
-                var data = {
-                    pekerja : this.Pekerja,
-                    Nama : this.Pekerja["Nama"],
-                    Gaji : this.Pekerja["Gaji"]
-                }
-                this.tbody.innerHTML = Mustache.render(this.template.innerHTML, data)
-                this.inputNama.value = ""
-                this.inputGaji.value = ""
+var pekerja = (function(){
+    let Pekerja = [{"Nama" : "abdi", "Gaji" : "20000000"}]
+    
+        let inputGaji = document.querySelector("#inputGaji")
+        let inputNama = document.querySelector("#inputName")
+        let btnSubmit = document.querySelector("#btnSubmit")
+        let template = document.querySelector("#template")
+        let table = document.querySelector("#output")
+        let tbody = table.querySelector("#body")
+        let tr = document.querySelectorAll("tr")
+        let btnDelete = document.querySelectorAll(".delete")
+
+        btnSubmit.addEventListener('click', pushKePekerja)
+        //delegate multiple button
+        tbody.addEventListener('click', function(e) {
+            if(e.target.nodeName = "BUTTON"){
+                deletePekerja(e)
             }
-        },
+        })
+        _tampilkan()
+
+      function pushKePekerja(nama,gaji){
+        let obj = {}
+        let Nama = (typeof nama === "string") ? nama : inputNama.value
+        let Gaji = (typeof gaji === "string") ? gaji : inputGaji.value
+        obj["Nama"] = Nama
+        obj["Gaji"] = Gaji
+        Pekerja.push(obj)
+        _tampilkan()
+      }
+
+     function deletePekerja(value){
+        var i;
+        if(typeof value === "number"){
+            i = value
+        }else{
+            //find the closest tr element from e.target
+            i = value.target.closest("tr")
+            //removing a child from tbody
+            tbody.removeChild(i)
+
+            let parent = tbody;
+            let index = Array.prototype.indexOf.call(parent.children, i)
+            i = index;
+        }
+        Pekerja.splice(i,1)
+        _tampilkan()
     }
-    pekerja.init();
-})()
+    
+    function _tampilkan(){
+        if(Pekerja.length != 0){
+            var data = {
+                pekerja : Pekerja,
+                Nama : Pekerja["Nama"],
+                Gaji : Pekerja["Gaji"]
+            }
+            tbody.innerHTML = Mustache.render(template.innerHTML, data)
+            inputNama.value = ""
+            inputGaji.value = ""
+        }
+    }
+    return {
+        tambahPekerja : pushKePekerja,
+        deletePekerja : deletePekerja
+    }
+})();
